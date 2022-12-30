93cc96bd7802e75b2adec9be29eb410a8b66dda7
Marat-Tim <rurikcat@gmail.com>
2022-12-30 16:38:09 +0300

    Добавил в классы наборы методов с комментариями без реализаций

---
+++ src/File.java
@@ -0,0 +1,25 @@
+import java.util.Set;
+
+class File {
+    File(String path) {
+
+    }
+
+    /**
+     * Возвращает весь текст из файла.
+     * @return Весь текст из файла.
+     */
+    String getText() {
+
+    }
+
+    /**
+     * Возвращает все зависимости данного файла в виде множества.
+     * @return Все зависимости данного файла.
+     */
+    Set<String> getDependencies() {
+
+    }
+
+
+}
