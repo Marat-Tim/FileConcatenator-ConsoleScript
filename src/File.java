e0bbf3fb7aaa7f84503178e5b17eafb4584e02ea
Marat-Tim <rurikcat@gmail.com>
2023-01-07 20:14:04 +0300

    убрал лишние импорты

--- src/File.java
+++ src/File.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Обертка над путем к файлу.
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 public class Main {
     public static void main(String[] args) {
         UI.start();
